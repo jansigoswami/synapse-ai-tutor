8666114e3b83c99a0b2f4ec47a531c8aeda0dad5
JansiTech <jansi.tech44@gmail.com>
2025-10-04 01:07:40 +0530

    getting on same page

--- synapse/src/app/page.tsx
+++ synapse/src/app/page.tsx
@@ -1,7 +1,5 @@
 import ClientSynapse from "./ClientSynapse";
-import ClientSynapse from "./ClientSynapse";
 
 export default function Home() {
-  return <ClientSynapse />;
   return <ClientSynapse />;
 }
